5aaba293e95c9aa758d0b1898166aea225a5fa55
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-03-22 19:35:01 +0900

    feat: infra周り整理

---
+++ mysql/initdb.d/02-create-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `merlin`.`users`
+(
+    id int not null primary key,
+	name varchar(255) not null,
+    sex varchar(255) not null,
+    enabled boolean not null,
+	created_at int not null,
+	updated_at int not null
+)
---
+++ mysql/initdb.d/03-users.sql
@@ -0,0 +1,9 @@
+INSERT INTO `merlin`.users (
+    id,name,sex,enabled,created_at,updated_at
+)
+VALUES
+    (1,'山田太郎','man',true,555555,555555),
+    (2,'佐藤二郎','man',true,555555,555555),
+    (3,'田中三郎','man',true,555555,555555),
+    (4,'林花子','woman',true,555555,555555),
+    (5,'村田涼子','woman',true,555555,555555);
