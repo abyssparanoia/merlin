ad6e12e3cf2dbbe668487faa28c47efeabd7e5c5
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-03-22 17:15:28 +0900

    fix: type修正

--- src/model/user.go
+++ src/model/user.go
@@ -1,13 +1,11 @@
 package model
 
-import "time"
-
 // User ... ユーザーモデル
 type User struct {
-	ID        int64     `db:"id" json:"id"`
-	Name      string    `db:"name" json:"name"`
-	Sex       string    `db:"sex" json:"sex"`
-	Enabled   bool      `db:"enabled" json:"enabled"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	ID        int64  `db:"id" json:"id"`
+	Name      string `db:"name" json:"name"`
+	Sex       string `db:"sex" json:"sex"`
+	Enabled   bool   `db:"enabled" json:"enabled"`
+	CreatedAt int64  `db:"created_at" json:"created_at"`
+	UpdatedAt int64  `db:"updated_at" json:"updated_at"`
 }
